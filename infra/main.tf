4ef7809d107c0eb0eb313661fa0ae464522e88f6
padmaja <Padmaja.kvn09@gmail.com>
2026-05-05 12:11:28 +0530

    Enable VPC configuration for Lambda function

--- infra/main.tf
+++ infra/main.tf
@@ -528,10 +528,10 @@ resource "aws_lambda_function" "trigger" {
   memory_size      = 256
   
   # VPC configuration for Lambda (optional, removed for simplicity in dev)
-  # vpc_config {
-  #   subnet_ids         = aws_subnet.private[*].id
-  #   security_group_ids = [aws_security_group.lambda.id]
-  # }
+  vpc_config {
+    subnet_ids         = aws_subnet.private[*].id
+    security_group_ids = [aws_security_group.lambda.id]
+  }
   
   environment {
     variables = {
